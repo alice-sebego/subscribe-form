f9a58584ff2b3c8cdc323ab6f98503e0acb1c336
Alice SEBEGO <alice.etvotrecom@gmail.com>
2021-05-07 23:24:09 +0200

    Create a regex and conditions in order to validate username

--- src/app.js
+++ src/app.js
@@ -1,3 +1,24 @@
 import _ from 'lodash';
 
-console.log("Hello :)");
+let $user = document.querySelector("#user");
+let $email = document.querySelector("#email");
+let $password = document.querySelector("#password");
+let $confirmPassword = document.querySelector("#confirm-password");
+
+// Regex
+const regexUser = /^[a-zA-Z\d\S]{3,15}$/;
+
+$user.addEventListener("change", (e) =>{
+    
+    e.preventDefault();
+
+    if($user.value){
+        if($user.value.length > 3 && $user.value.length < 15 && regexUser.test($user.value) === true ){
+            console.log("Yep, username length and regex "+ $user.value + " is OK")
+        } else {
+            console.log("Your username must have a length beetween 3 and 15 strings & doesn't have a space");
+        }
+    }
+})
+
+/* regex username || ^[a-zA-Z\d\S]{3,15}$ */
