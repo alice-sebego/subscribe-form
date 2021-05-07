0faf868fe00f55d5c082c1214e1bb935ad8f1501
Alice SEBEGO <alice.etvotrecom@gmail.com>
2021-05-08 00:27:38 +0200

    Handle display of info for input's username depending its validation

--- src/app.js
+++ src/app.js
@@ -5,18 +5,31 @@ let $email = document.querySelector("#email");
 let $password = document.querySelector("#password");
 let $confirmPassword = document.querySelector("#confirm-password");
 
-// Regex
+const $userField = document.querySelector("#userField");
+
+// Regex List
 const regexUser = /^[a-zA-Z\d\S]{3,15}$/;
 
 $user.addEventListener("change", (e) =>{
-    
+
     e.preventDefault();
 
     if($user.value){
+
+        let $infoUser = document.createElement("p");
+        $infoUser.setAttribute("id", "infoUser");
+        $infoUser.classList.add("info");
+        $userField.appendChild($infoUser);
+
         if($user.value.length > 3 && $user.value.length < 15 && regexUser.test($user.value) === true ){
-            console.log("Yep, username length and regex "+ $user.value + " is OK")
+            
+            $infoUser.classList.add("infoRight");
+            $infoUser.innerHTML = `Votre pseudo <strong>${user.value}</strong> est valide`;
+            
         } else {
-            console.log("Your username must have a length beetween 3 and 15 strings & doesn't have a space");
+            $infoUser.classList.add("infoWrong");
+            $infoUser.innerHTML = `Votre pseudo <strong>${user.value}</strong> doit contenir entre 3 et 15 caractères & ne pas contenir d'espace`;
+           
         }
     }
 })
