6726083e51aa8c7088dd8f2bcaaf8772ff3d29bb
Alice SEBEGO <alice.etvotrecom@gmail.com>
2021-05-09 00:00:36 +0200

    Handle email's input of user

--- src/js/app.js
+++ src/js/app.js
@@ -14,10 +14,13 @@ const $passwordField = document.querySelector("#passwordField");
 const $confirmPassWField = document.querySelector("#confirmPassWField");
 
 const $infoUser = document.querySelector("#infoUser");
+const $infoMail = document.querySelector("#infoMail");
 
 // Regex List
 const regexUser = /^[a-zA-Z\d\S]{3,15}$/;
+const regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
 
+// Handle Username
 $user.addEventListener("change", (e) =>{
 
     e.preventDefault();
@@ -25,7 +28,6 @@ $user.addEventListener("change", (e) =>{
 
     if($user.value){
 
-        $infoUser.setAttribute("id", "infoUser");
         $infoUser.classList.add("info");
         $userField.appendChild($infoUser);
 
@@ -34,15 +36,44 @@ $user.addEventListener("change", (e) =>{
            regexUser.test($user.value) === true ){
             
             $infoUser.classList.add("infoRight");
-            $infoUser.innerHTML = `Votre pseudo <strong>${user.value}</strong> est valide`;
+            $infoUser.innerHTML = `Votre pseudo <strong>${$user.value}</strong> est valide`;
             util.removeElement($infoUser);
 
         } else {
             
             $infoUser.classList.add("infoWrong");
-            $infoUser.innerHTML = `Votre pseudo <strong>${user.value}</strong> doit contenir entre 3 et 15 caractères & ne pas contenir d'espace`;
+            $infoUser.innerHTML = `Votre pseudo <strong>${$user.value}</strong> doit contenir entre 3 et 15 caractères & ne pas contenir d'espace`;
             util.removeElement($infoUser);
         }
+    }
+
+});
+
+// Handle user's email
+$email.addEventListener("change", (e) =>{
+
+    e.preventDefault();
+    $infoMail.innerHTML = "";
+
+    if($email.value){
+
+        $infoMail.classList.add("info");
+        $mailFiel.appendChild($infoMail);
+
+        if(regexEmail.test($email.value) === true){
+
+            $infoMail.classList.add("infoRight");
+            $infoMail.innerHTML = `Votre e-mail <strong>${$email.value}</strong> est valide`;
+            util.removeElement($infoMail);
+
+        } else {
+
+            $infoMail.classList.add("infoWrong");
+            $infoMail.innerHTML = `Votre e-mail <strong>${$email.value}</strong> n'est pas valide`;
+            util.removeElement($infoMail);
+
+        }
+
     }
 
 });
